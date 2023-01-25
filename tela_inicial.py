62baf455ff0a23b1d7bf23cac4252a6dc6dc0a2a
steniofonsecaa <stenioomoraes12@gmail.com>
2023-01-25 00:48:31 -0300

    Alterações

--- tela_inicial.py
+++ tela_inicial.py
@@ -7,32 +7,19 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Jogo Snake")
 
-#Título do jogo
+#Fonte texto do título
 titulo = pygame.font.SysFont("arial", 50)
-titulo_texto = titulo.render("Snake Classic", True, (255, 0, 0))
-titulo_pos = titulo_texto.get_rect(center=(screen.get_width()/2, screen.get_height()/2 - 100))
-screen.blit(titulo_texto, titulo_pos)
 
-#Indicador do menu
-seta = pygame.Surface((25, 10))
-seta.fill((255, 255, 255))
-seta_pos = seta.get_rect(center=(screen.get_width()/2 - 70, screen.get_height()/2))
-screen.blit(seta, seta_pos)
-
-#Texto menu
+#Texto menu e fonte
 opcoes = ["Jogar", "Opções", "Sair"]
 fonte = pygame.font.SysFont("arial", 30)
-for i, opcao in enumerate(opcoes):
-    texto_menu = fonte.render(opcao, True, (255, 255, 255))
-    texto_menu_pos = texto_menu.get_rect(center=(screen .get_width()/2, screen.get_height()/2 + i * 50))
-    screen.blit(texto_menu, texto_menu_pos)
 
-#Texto de desenvolvimento
+#Fonte do texto de desenvolvimento
 desenvolvido = pygame.font.SysFont("arial", 15)
-desenvolvido_texto = desenvolvido.render("Desenvolvido por Stenio Fonsêca", True, (255, 255, 255))
-desenvolvido_pos = desenvolvido_texto.get_rect(center=(screen.get_width()/2, screen.get_height() - 30))
-screen.blit(desenvolvido_texto, desenvolvido_pos)
 
+#Indicador de seleção do menu
+indicador = pygame.Surface((25, 10))
+indicador.fill((255, 255, 255))
 
 #Loop da tela inicial
 while True:
@@ -40,4 +27,27 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        pygame.display.update()
+    
+    #Desenhando o título
+    titulo_texto = titulo.render("Snake Classic", True, (255, 0, 0))
+    titulo_pos = titulo_texto.get_rect(center=(screen.get_width()/2, screen.get_height()/2 - 100))
+    screen.blit(titulo_texto, titulo_pos)
+    
+    #Desenhando o texto de desenvolvimento
+    desenvolvido_texto = desenvolvido.render("Desenvolvido por Stenio Fonsêca", True, (255, 255, 255))
+    desenvolvido_pos = desenvolvido_texto.get_rect(center=(screen.get_width()/2, screen.get_height() - 30))
+    screen.blit(desenvolvido_texto, desenvolvido_pos)
+
+    #Desenhando o indicador de seleção do menu fazendo piscar devagar
+    if pygame.time.get_ticks() % 1000 < 500:
+        indicador_pos = indicador.get_rect(center=(screen.get_width()/2 - 100, screen.get_height()/2))
+        screen.blit(indicador, indicador_pos)
+    
+    
+    #Desenhando o menu
+    for i, opcao in enumerate(opcoes):
+        texto_menu = fonte.render(opcao, True, (255, 255, 255))
+        texto_menu_pos = texto_menu.get_rect(center=(screen .get_width()/2, screen.get_height()/2 + i * 50))
+        screen.blit(texto_menu, texto_menu_pos)
+        
+    pygame.display.update()
